61d8651ae8b4f90dfaa23fa7a8a100378d4195bb
Ashish Tamang <your-email@example.com>
2026-02-01 23:17:37 +0545

    feat: featured and products page, pagination

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
