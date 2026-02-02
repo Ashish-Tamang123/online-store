ad997375bd5c6f258b52a178fbb73673f2739be1
Ashish Tamang <your-email@example.com>
2026-02-02 23:31:38 +0545

    feat: filter and sorting in products page

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product
 from django.core.paginator import Paginator
+from .forms import ProductFilterForm
 
 def home(request):
     featured_products = Product.objects.filter(featured=True).order_by('-created_at')[:8]
@@ -10,13 +11,39 @@ def home(request):
     return render(request, "store/home.html", context)
 
 def products(request):
-    products = Product.objects.all().order_by('-created_at')
-    products_paginator = Paginator(products, 8)
+    products = Product.objects.all()
+    filter_form = ProductFilterForm(request.GET)
 
+    if filter_form.is_valid():
+        if filter_form.cleaned_data.get('name'):
+            products = products.filter(name__icontains = filter_form.cleaned_data.get('name'))
+
+        if filter_form.cleaned_data.get('min_price'):
+            products = products.filter(price__gte = filter_form.cleaned_data.get('min_price'))
+
+        if filter_form.cleaned_data.get('max_price'):
+            products = products.filter(price__lte = filter_form.cleaned_data.get('max_price'))
+
+        if filter_form.cleaned_data.get('categories'):
+            products = products.filter(categories__in = filter_form.cleaned_data.get('categories')).distinct()
+
+        sorting_key = filter_form.cleaned_data.get('sorting_key')
+        if sorting_key == 'price_asc':
+            products = products.order_by('price')
+        elif sorting_key == 'price_desc':
+            products = products.order_by('-price')
+        elif sorting_key == 'oldest':
+            products = products.order_by('created_at')
+        else:
+            products = products.order_by('-created_at')
+
+        
+    products_paginator = Paginator(products, 8)   
     page_number = request.GET.get('page')
     page_obj = products_paginator.get_page(page_number)
 
     context = {
         'products': page_obj,
+        'filter_form': filter_form,
     }
     return render(request, "store/products.html", context)
